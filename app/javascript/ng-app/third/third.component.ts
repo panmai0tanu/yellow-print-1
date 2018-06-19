091350d30a6b78a51a51f920e077c09c8f0e1601
panmai0tanu <panmai_tanu@cmu.ac.th>
2018-06-19 16:27:14 +0700

    first

---
+++ app/javascript/ng-app/third/third.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import templateString from './third.html';
+import { UserService } from '../user/user.service';
+import { I18n } from '../utils/i18n';
+
+@Component({
+  template: templateString,
+  providers: [ UserService, I18n ]
+})
+export class ThirdComponent {
+  user: any;
+  users: any[];
+  greeting: string;
+  usersColumns: any[] = [
+    { prop: 'email' },
+    { prop: 'role' }
+  ];
+
+  constructor(private userService: UserService, private i18n: I18n) {
+    this.greeting = i18n.t('hello');
+    this.userService.all().subscribe((users: any[]) => {
+      this.users = users;
+    });
+    this.userService.find(1).subscribe((user: object) => {
+      this.user = user;
+    });
+  }
+}
